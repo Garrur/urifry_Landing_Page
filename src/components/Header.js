e03b75b1df8f465a6ec047bf1690bf3c0b265b52
Utkarsh Raj <uraj33175@gmail.com>
2024-06-08 13:22:42 +0530

    err

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,10 @@ const Header = () => {
     <div className="main-head-section">
       <div className="left-text-heading-section">
         <img className="blur-head-first" src="dir/images/blur1.png" alt="" />
-        <h1 data-aos="fade-up" className={darkMode ? "dark-text" : ""}>
+        <h1  className={darkMode ? "dark-text" : ""}>
           make the best financial decisions
         </h1>
-        <p data-aos="fade-up" className={darkMode ? "dark-text" : ""}>
+        <p  className={darkMode ? "dark-text" : ""}>
           Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
           faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
         </p>
