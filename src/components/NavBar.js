b1e6f1fe5a61c5428e37fda4d849fb313ab4502a
Utkarsh Raj <uraj33175@gmail.com>
2024-06-08 11:03:39 +0530

    css new

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { ThemeContext } from './ThemeContext';
-import ToggleButton from './ToggleButton';
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+import ToggleButton from "./ToggleButton";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,27 +13,50 @@ const NavBar = () => {
   return (
     <nav>
       <div className="logo-nav">
-      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle menu">
-      <i className="fa-solid fa-bars white-icon"></i>
-    </button>
+        <button
+          className="menu-toggle"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <i
+            className={`fa-solid fa-bars ${
+              darkMode ? "white-icon" : "black-icon"
+            }`}
+          ></i>
+        </button>
+
         <img
-          src={darkMode ? 'dir/images/logo-white.png' : 'dir/images/logo.png'}
+          src={darkMode ? "dir/images/logo-white.png" : "dir/images/logo.png"}
           alt="uiFry Logo"
           className="logo-nav-img"
         />
       </div>
 
-      <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <button className="close-btn" onClick={toggleMenu} aria-label="Close menu">
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <button
+          className="close-btn"
+          onClick={toggleMenu}
+          aria-label="Close menu"
+        >
           <i className="fa-solid fa-xmark white-icon"></i>
         </button>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Features</a></li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Pricing</a>
+        </li>
+        <li>
+          <a href="#">Features</a>
+        </li>
       </ul>
-      <ToggleButton/>
-      <button className={`nav-button ${darkMode ? 'dark-text' : ''}`}>Download</button>
+      <ToggleButton />
+      <button className={`nav-button ${darkMode ? "dark-text" : ""}`}>
+        Download
+      </button>
     </nav>
   );
 };
